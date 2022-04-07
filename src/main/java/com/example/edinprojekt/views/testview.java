955dbd43c034266eb5b67d7bb1120f98f10e8745
edinhaurdic <edinhaurdic@gmail.com>
2022-04-07 13:54:30 +0200

    Thymeleaf mm.

--- src/main/java/com/example/edinprojekt/views/testview.java
+++ src/main/java/com/example/edinprojekt/views/testview.java
@@ -4,9 +4,11 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("/")
+@Route("/test")
 public class testview extends VerticalLayout {
 
-    H1 title = new H1("");
+
+
+    H1 title = new H1("Hello");
 
 }
